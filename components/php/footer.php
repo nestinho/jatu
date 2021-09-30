2cfe4685bdeb06b3eb47a332c0767c491f2def0c
nestinho <nestory2008@gmail.com>
2021-09-30 13:18:55 +0300

    hello world

---
+++ components/php/footer.php
@@ -0,0 +1,87 @@
+<footer>
+    
+    <div class="row">
+  <div class="column2">
+    <h2>Companies</h2>
+    <a href="#jatutechhub" id="footerlink">JATU TECHHUB</a> <br><br>
+    <a href="#jatutalk" id="footerlink">JATU TALK</a> <br><br>
+    <a href="#jatupesa" id="footerlink">JATU PESA</a> <br><br>
+    <a href="#jatusaccoss" id="footerlink">JATU SACCOSS</a> <br><br>
+    <a href="#jaturestaurant" id="footerlink">JATU RESTAURANT</a> <br><br>
+    <a href="#jatukilimo" id="footerlink">JATU KILIMO</a> <br><br>
+    <a href="#hyperalliance" id="footerlink">HYPER ALLIANCE</a> <br><br>
+    <a href="#ridefastsafari" id="footerlink">RIDE FAST SAFARI</a>
+  </div>
+
+  <div class="column2">
+    <h2>Quick Links</h2>
+    <a href="#home" class="active" id="footerlink">Home</a> <br><br>
+  <a href="#about" id="footerlink">About Us</a> <br><br>
+  <a href="#services" id="footerlink">Services</a> <br><br>
+  <a href="#members" id="footerlink">Members</a> <br><br>
+  <a href="#blog" id="footerlink">Organization Structure</a> <br><br>
+  <a href="#contact" id="footerlink">Contact Us</a> <br><br>
+
+  </div>
+
+  <div class="column2">
+    <h2>Get In Touch</h2>
+    <i class="fa fa-facebook" style="color: blue; font-size: 30px;"></i> &nbsp
+    <i class="fa fa-twitter" style="color: lightblue; font-size: 30px;"></i> &nbsp
+    <i class="fa fa-instagram" style="color: blueviolet; font-size: 30px;"></i> &nbsp
+    <i class="fa fa-youtube" style="color: red; font-size: 30px;"></i> <br><br>
+    <hr>
+    <h2>Address</h2>
+    <address>
+      HEAD OFFICE,<br>
+      JATU PUBLIC LIMITED COMPANY, <br>
+      P.O BOX 42155, DAR ES SALAAM. <br><br>
+
+      PSSSF House 11th & 06th Floor,<br>
+      Samora Avenue/ Morogoro Road. <br>
+      Ilala CBD, Dar es Salaam – Tanzania</address> 
+
+      <div class="row">
+        <div class="column3">
+      <h2>Call Us:</h2> 
+      <i class="fa fa-phone"></i>
+      <strong>+255 (0) 758 396 767</strong> <br>
+    </div>
+    <div class="column3">
+      <h2>Email Us:</h2> 
+      <i class="fa fa-envelope"></i>
+      <strong>info@jatu.co.tz</strong>
+    </div>
+      </div>
+  </div>
+</div>
+
+<div>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.5856173699535!2d39.28418817513762!3d-6.820146888529782!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x185c4b05b0ee8de1%3A0x65bf4eaff8c8a90e!2sJATU%20PLC!5e0!3m2!1ssw!2stz!4v1632119920859!5m2!1ssw!2stz" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy">
+      </iframe>
+</div>
+
+
+<div class="row">
+  <div class="column2">
+    <strong>
+    &copy 2021  JATU PLC.
+  </strong>
+  </div>
+  <div class="column2">
+     <strong> Powered By TechHub.</strong>
+  </div>
+  <div class="column2">
+    <strong>
+    All Rights Reserved.
+  </strong>
+  </div>
+</div>
+
+
+  </footer>
+
+  <script type="text/javascript" href="C:\Users\clavery\Desktop\jatuplc\components\js\home.js"></script>
+
+  </body>
+</html>
---
+++ components/php/header.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+          <!-- External CSS -->
+          <link rel="stylesheet" type="text/css" href="C:\Users\clavery\Desktop\jatuplc\components\css\home.css">
+          <script type="text/javascript" src="C:\Users\clavery\Desktop\jatuplc\components\js\home.js"></script>
+
+          <!-- Load an Icon library -->
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+         <title href="https://jatu.co.tz">JATU PLC</title>
+
+         <style type="text/css">
+         
+         </style>
+
+  </head>
+  <body>
+    
+    <header class="navheader">
+      <img src="C:\Users\clavery\Desktop\jatuplc\images\logojatu.jpg" alt="logojatu" id="juulogo">
+      <h1 align="center" style="color: #ff6600;">JATU PLC</h1>
+    <h5 align="center" style="color: #ff6600;">"Jenga Afya Tokomeza Umaskini"</h5>
+      
+<div class="topnav" id="myTopnav">
+  <a href="#home" class="active">Home</a>
+  <a href="#about">About Us</a>
+  <a href="#services">Services</a>
+  <a href="#members">Members</a>
+  <a href="#blog">Organization Structure</a>
+  <a href="#contact">Contact Us</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+
+    </header>
